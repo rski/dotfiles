5670ea5ebe395a90519b4b8847da445835771f27
Romanos <rski@users.noreply.github.com>
2015-12-23 12:00:36 +0200

    install_all.sh copies files from submodules/ to  wherever needed

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+NVIM_FOLDER="nvim/.config/nvim"
+# only a single file is needed from some submodules' repo
+# so we put the submodules in submodules/ and then copy the file needed
+function submodule_install { 
+  mkdir -p $NVIM_FOLDER/autoload
+  cp submodules/vim-plug/plug.vim $NVIM_FOLDER/autoload/
+}
+
+submodule_install
+
 TARGET=$HOME
 programs=('terminator' 
           'aliases'
