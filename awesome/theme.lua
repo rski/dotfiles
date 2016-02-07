03e42a47c972be9a0f543a7fc9e69cf95f1bb13a
rski <rom.skiad@gmail.com>
2016-02-07 21:43:33 +0200

    add fallback to the default theme

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -5,11 +5,17 @@ awful.util = require("awful.util")
 --so we do a similar thing which allows overriding the theme values before it is loaded by beautiful
 local themeblock = loadfile("/usr/share/awesome/themes/powerarrow-darker/theme.lua")
 
+if themeblock == nil then
+  themeblock = loadfile("/usr/share/awesome/themes/default/theme.lua")
+end
+
 themeblock()
 theme = themeblock()
 theme.tasklist_disable_icon = true
 theme.wallpaper = awful.util.getdir("config") .. "/wall.png"
-theme.awesome_icon = "/usr/share/awesome/themes/powerarrow-darker/icons/submenu.png"
+if theme.awesome_icon == nil then
+  theme.awesome_icon = "/usr/share/awesome/themes/powerarrow-darker/icons/submenu.png"
+end
 theme.taglist_font="Tengwar Annatar 8"
 
 
